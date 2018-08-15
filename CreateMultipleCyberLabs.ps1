bc26a01965eefffa7c4eb341b481ba11afe6d08b
Luca Bolognese <lucabol@microsoft.com>
2018-08-15 07:09:45 +0000

    Print arglist

--- CreateMultipleCyberLabs.ps1
+++ CreateMultipleCyberLabs.ps1
@@ -39,9 +39,10 @@ ForEach ($lab in $config) {
   $errorFile = $lab.DevTestLabName + $dateString + ".err.txt"
   $errorFilePath = Join-Path $errorFolder $errorFile
 
-  $argList = "-File ""$executable"" -DevTestLabName $($lab.DevTestLabName) -ResourceGroupName $($lab.ResourceGroupName) -StorageAccountName $($lab.StorageAccountName) -StorageContainerName $($lab.StorageContainerName) -StorageAccountKey ""$($lab.StorageAccountKey)"" -ShutDownTime $($lab.ShutDownTime) -TimeZoneId ""$($lab.TimeZoneId)"" -LabRegion ""$($lab.LabRegion)"" -LabOwners ""$($lab.LabOwners)"" -LabUsers ""$($lab.LabUsers)"""
+  $argList = "-File ""$executable"" -DevTestLabName $($lab.DevTestLabName) -ResourceGroupName $($lab.ResourceGroupName) -StorageAccountName $($lab.StorageAccountName) -StorageContainerName $($lab.StorageContainerName) -StorageAccountKey ""$($lab.StorageAccountKey)"" -ShutDownTime $($lab.ShutDownTime) -TimeZoneId ""$($lab.TimeZoneId)"" -LabRegion ""$($lab.LabRegion)"" -LabOwners $($lab.LabOwners) -LabUsers $($lab.LabUsers)"
 
   Write-Output "$count : Creating lab $($lab.DevTestLabName)"
+  Write-Output "$argList"
   $procs += Start-Process "powershell.exe" -PassThru -WindowStyle $WindowsStyle -RedirectStandardError $errorFilePath -ArgumentList $argList -WorkingDirectory $scriptFolder
   Start-Sleep -Seconds ($MinutesToNextLabCreation * 60)
   $count += 1
