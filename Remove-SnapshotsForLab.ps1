7b863f2d2364f452c4a603e99f73ffd4495c5685
Luca Bolognese <lucabol@microsoft.com>
2018-10-08 09:17:06 +0200

    REFACT: Moved remove snapshots to new architecture

--- Remove-SnapshotsForLab.ps1
+++ Remove-SnapshotsForLab.ps1
@@ -4,15 +4,23 @@ param
   [string] $DevTestLabName,
 
   [Parameter(Mandatory=$true, HelpMessage="RG of lab to remove")]
-  [string] $ResourceGroupName
+  [string] $ResourceGroupName,
+
+  [Parameter(valueFromRemainingArguments=$true)]
+  [String[]]
+  $rest = @()
 )
+
+$ErrorActionPreference = "Stop"
+
+. "./Utils.ps1"
 
 Write-Host "Removing snapshots for lab $DevTestLabName in $ResourceGroupName"
 
 $snapshots = Get-AzureRmResource -ResourceType 'Microsoft.DevTestLab/labs/customImages' -ResourceName $DevTestLabName -ResourceGroupName $ResourceGroupName -ApiVersion '2016-05-15'
 
 if(-not $snapshots) {
-  Write-Error "No snapshots to remove in $DevTestLabName"
+  Write-Host "No snapshots to remove in $DevTestLabName"
   exit
 }
 
@@ -22,9 +30,4 @@ $snapshots | ForEach-Object {
   $jobs += Remove-AzureRmResource -AsJob -ResourceId $_.ResourceId -Force -ApiVersion '2016-05-15'
 }
 
-$jobCount = $jobs.Count
-Write-Host "Waiting for $jobCount snapshots deletion jobs to complete for $DevTestLabName"
-foreach ($job in $jobs){
-    $jobOutput = Receive-Job $job -Wait
-}
-Remove-Job -Job $jobs
+Wait-JobWithProgress -secTimeout (1 * 60 * 60) -jobs $jobs
--- Remove-SnapshotsForLabs.ps1
+++ Remove-SnapshotsForLabs.ps1
@@ -1,37 +1,13 @@
 param
 (
     [Parameter(Mandatory=$false, HelpMessage="Configuration File, see example in directory")]
-    [string] $ConfigFile = "config.csv",
-
-    [Parameter(Mandatory=$false, HelpMessage="How many minutes to wait before starting the next parallel lab deletion")]
-    [int] $MinutesToNextLabDeletion =  2
-
+    [ValidateNotNullOrEmpty()]
+    [string] $ConfigFile = "configTest.csv"
 )
 
+$error.Clear()
+$ErrorActionPreference = "Stop"
 
-$scriptFolder = Split-Path $Script:MyInvocation.MyCommand.Path
-$executable = Join-Path $scriptFolder "Remove-SnapshotsForLab.ps1"
+. "./Utils.ps1"
 
-# Check we're in the right directory
-if (-not (Test-Path $executable)) {
-  Write-Error "Unable to find $executable ...  unable to proceed."
-  return
-}
-
-$config = Import-Csv $ConfigFile
-
-$jobs = @()
-
-ForEach ($lab in $config) {
-  Write-Host "Starting job to delete snapshot for lab $($lab.DevTestLabName)"
-  $jobs += Start-Job -Name $lab.DevTestLabName -FilePath $executable -ArgumentList $lab.DevTestLabName, $lab.ResourceGroupName
-  Start-Sleep -Seconds 30
-}
-
-$jobCount = $jobs.Count
-Write-Host "Waiting for $jobCount snapshots deletion jobs to complete"
-foreach ($job in $jobs){
-    $jobOutput = Receive-Job $job -Wait
-    Write-Host $jobOutput
-}
-Remove-Job -Job $jobs
+"./Remove-SnapshotsForLab.ps1" | Invoke-ForEachLab -ConfigFile $ConfigFile -SecondsBetweenLoop 2
