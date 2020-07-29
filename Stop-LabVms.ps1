fc4763b26eaa2804a2b081092448b5c86cc55ea0
Peter Hauge <phauge@microsoft.com>
2020-07-28 22:14:46 -0400

    Update stop script so we always try to stop

--- Stop-LabVms.ps1
+++ Stop-LabVms.ps1
@@ -23,15 +23,15 @@ if (-not $existingLab) {
 }
 
 # Get only the running VMs
-$runningVms = Get-AzDtlVm -Lab $existingLab -Status "Running"
+$vms = Get-AzDtlVm -Lab $existingLab
 
 # Scope to a smaller set if needed
 if ($VmSettings) {
-    $runningVms = $runningVms | Where-Object { $_.Name -in ($VmSettings | Select -ExpandProperty imageName) }
+    $vms = $vms | Where-Object { $_.Name -in ($VmSettings | Select -ExpandProperty imageName) }
 }
 
-if (-not $runningVms) {
-  Write-Output "'$DevTestLabName' doesn't contain any running VMs"
+if (-not $vms) {
+  Write-Output "'$DevTestLabName' doesn't contain any VMs"
   return
 }
 
@@ -39,20 +39,12 @@ Write-Output "Stopping VMs in $DevTestLabName in RG $ResourceGroupName ..."
 Write-Output "This might take a while ..."
 
 $jobs = @()
-$runningVms | ForEach-Object {
+$vms | ForEach-Object {
 
   $sb = {
-    # Workaround for https://github.com/Azure/azure-powershell/issues/9448
-    $Mutex = New-Object -TypeName System.Threading.Mutex -ArgumentList $false, "Global\AzDtlLibrary"
-    $Mutex.WaitOne() | Out-Null
-    $rg = Get-AzResourceGroup | Out-Null
-    $Mutex.ReleaseMutex() | Out-Null
-
-    Stop-AzDtlVm -Vm $Using:_
+    Stop-AzDtlVm -Vm $_
   }
   $jobs += Start-RSJob -ScriptBlock $sb -Name $_.Name -ModulesToImport $AzDtlModulePath
-
-  Start-Sleep -Seconds 2
 }
 
 Wait-RSJobWithProgress -secTimeout (2 * 60 * 60) -jobs $jobs
